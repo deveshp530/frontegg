6138e5417e1cd85f5feaf7cd7849b0dc03bd4ed7
Devesh Patel <deveshp530@gmail.com>
2022-09-26 19:37:15 -0700

    assessment done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import { FronteggProvider } from "@frontegg/react";
+
+const contextOptions = {
+  baseUrl: "https://app-sht1osnoqhoj.frontegg.com",
+  clientId: "b5603634-e653-4528-a205-45dfbb50d6f9",
+};
+
+ReactDOM.render(
+  <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
     <App />
-  </React.StrictMode>
+  </FronteggProvider>,
+  document.getElementById("root")
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
